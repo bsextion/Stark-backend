a2ba0d4d0e89f1fa858cdcbfd78dd937680ff321
Brooke <bsextion@gmail.com>
2018-09-02 23:53:14 -0500

    updated

--- src/main/java/com/example/controllers/EmployeeController.java
+++ src/main/java/com/example/controllers/EmployeeController.java
@@ -17,56 +17,56 @@ import java.util.Optional;
     @RequestMapping("/got")
     public class EmployeeController {
 
-        @GetMapping("/")
+        @GetMapping("/index")
         public String index() {
             return "Hello there! I'm running.";
         }
 
-//    private EmployeeService employeeService;
-//    private EmployeeRepo employeeRepo;
-//
-//    @GetMapping(value = "/employee/{id}")
-//    public ResponseEntity <Employee> getEmployee ( HttpServletRequest httpServletRequest, @PathVariable("id") int id) {
-//
-//        try {
-//            Optional<Employee> employeeOptional = employeeService.getEmployeeById(id);
-//
-//            if (!employeeOptional.isPresent()) {
-//                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
-//
-//            } else {
-//                return new ResponseEntity<>(employeeService.getEmployeeById(id).get(), HttpStatus.OK);
-//            }
-//        }
-//            catch (NullPointerException e) {
-////            log.debug(e.getMessage())
-//                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-//
-//        }
-//
-//    }
-//
-//
-//
-//
-//    @PostMapping(value = "/employee/new")
-//    public ResponseEntity<String> addEmployee(HttpServletRequest httpServletRequest, @RequestBody Employee employee)
-//    {
-//        try {
-//            if (!employeeService.saveEmployee(employee)) {
-//
-//                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//            } else {
-//                return new ResponseEntity<>(HttpStatus.OK);
-//            }
-//
-//        }
-//        catch (IOException e) {
-////            log.debug(e.getMessage())
-//            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
-//        }
-//    }
-//
+    private EmployeeService employeeService;
+    private EmployeeRepo employeeRepo;
+
+    @GetMapping(value = "/employee/{id}")
+    public ResponseEntity <Employee> getEmployee ( HttpServletRequest httpServletRequest, @PathVariable("id") int id) {
+
+        try {
+            Optional<Employee> employeeOptional = employeeService.getEmployeeById(id);
+
+            if (!employeeOptional.isPresent()) {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+
+            } else {
+                return new ResponseEntity<>(employeeService.getEmployeeById(id).get(), HttpStatus.OK);
+            }
+        }
+            catch (NullPointerException e) {
+//            log.debug(e.getMessage())
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+
+        }
+
+    }
+
+
+
+
+    @PostMapping(value = "/employee/new")
+    public ResponseEntity<String> addEmployee(HttpServletRequest httpServletRequest, @RequestBody Employee employee)
+    {
+        try {
+            if (!employeeService.saveEmployee(employee)) {
+
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } else {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+
+        }
+        catch (IOException e) {
+//            log.debug(e.getMessage())
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+
 
 
 }
